d55480d370a0449fda50be1bb2599f87db50162c
Brittany Artimez <brittanyartimez@gmail.com>
2017-05-01 15:24:17 -0500

    fixes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
+# See http://guides.rubyonrails.org/routing.html#the-purpose-of-the-rails-router for more information
 Rails.application.routes.draw do
   # going to localhost:3000 should route us to our show all page
   root to: 'posts#index'
-  resources :posts, only: [:index, :new, :create, :show, :destroy]
-  # See http://guides.rubyonrails.org/routing.html#the-purpose-of-the-rails-router for more information
-  resources :posts do
-    resources :comments
+  resources :posts, only: [:index, :new, :create, :show, :destroy] do
+    resources :comments, only: [:create, :destroy]
   end
-  
+
 end
