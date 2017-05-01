3c6cbcebb633a3ed53daa30e90eb3008f34ac770
Brittany Artimez <brittanyartimez@gmail.com>
2017-05-01 18:21:56 -0500

    integrates bootstrap and sets up header navbar

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   # wire the other half of the post/comment relationship
   has_many :comments
 
+  # build the search method for our search posts form
   def self.search(search)
     where("title || body LIKE ?", "%#{search}%")
   end
