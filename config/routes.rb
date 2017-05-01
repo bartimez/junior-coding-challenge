90fefcc062851bd785e5ff09ac9fe434401902e3
Brittany Artimez <brittanyartimez@gmail.com>
2017-05-01 13:40:37 -0500

    adds single post get route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :new, :create, :destroy]
+  resources :posts, only: [:index, :new, :create, :show, :destroy]
   # See http://guides.rubyonrails.org/routing.html#the-purpose-of-the-rails-router for more information
 end
