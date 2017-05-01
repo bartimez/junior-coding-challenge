906f932f6529a35e58fd02f1fef7904f962768c5
Brittany Artimez <brittanyartimez@gmail.com>
2017-05-01 15:01:11 -0500

    adds index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # going to localhost:3000 should route us to our show all page
+  root to: 'posts#index'
   resources :posts, only: [:index, :new, :create, :show, :destroy]
   # See http://guides.rubyonrails.org/routing.html#the-purpose-of-the-rails-router for more information
 end
