8dbdecf17fa9396991ded6c6e5efb3c695c25ee0
Brittany Artimez <brittanyartimez@gmail.com>
2017-05-01 21:08:44 -0500

    more styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   # wire the other half of the post/comment relationship
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   # build the search method for our search posts form
   def self.search(search)
